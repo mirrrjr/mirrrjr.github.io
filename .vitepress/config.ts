fdbd24467c5aa51446d7cbc223ddb201396720c1
mirrrjr <mirrrrrr@yahoo.com>
2025-07-26 09:31:33 +0500

    add config

---
+++ .vitepress/config.ts
@@ -0,0 +1,59 @@
+import { defineConfig } from 'vitepress'
+import { getPosts } from './theme/serverUtils'
+
+// Number of articles per page
+const pageSize = 10
+
+const isProd = process.env.NODE_ENV === 'production'
+
+export default defineConfig({
+    title: 'Vitepress blog',
+    base: '/',
+    cacheDir: './node_modules/vitepress_cache',
+    description: 'vitepress,blog,blog-theme',
+    ignoreDeadLinks: true,
+    themeConfig: {
+        posts: await getPosts(pageSize),
+        website: 'https://github.com/airene/vitepress-blog-pure', //copyright link
+        // Comment repository address https://giscus.app/ Please overwrite after initializing according to the official instructions
+        comment: {
+            repo: 'airene/vitepress-blog-pure',
+            repoId: 'MDEwOlJlcG9zaXRvcnkzODIyMjY5Nzg',
+            categoryId: 'DIC_kwDOFshSIs4CpZga'
+        },
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Category', link: '/pages/category' },
+            { text: 'Archives', link: '/pages/archives' },
+            { text: 'Tags', link: '/pages/tags' },
+            { text: 'About', link: '/pages/about' }
+            // { text: 'Airene', link: 'http://airene.net' }  -- External link test
+        ],
+        search: {
+            provider: 'local'
+        },
+        //outline:[2,3],
+        outline: {
+            label: 'Article Summary'
+        },
+        socialLinks: [{ icon: 'github', link: 'https://github.com/mirrrjr/blog' }]
+    } as any,
+
+    srcExclude: isProd
+        ? [
+            '**/trash/**/*.md', // 排除所有 trash 目录
+            '**/draft/**/*.md', // 递归排除子目录
+            '**/private-notes/*.md', // 排除特定文件
+            'README.md'
+        ]
+        : ['README.md'],
+    vite: {
+        //build: { minify: false }
+        server: { port: 5000 }
+    }
+    /*
+      optimizeDeps: {
+          keepNames: true
+      }
+      */
+})
